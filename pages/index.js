d32c56a56728b2a37d965d0bfacada0dfde1486d
duongvanphi19 <duongvanphi19@gmail.com>
2022-11-12 14:07:21 +0700

    update favicon

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
      <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+      <link rel="icon" type="image/x-icon" href="/images/favicon.ico"/>
      </Head>
      </>
       )
