e6d541f806b587e947ede92435c5699ffa8d8840
duongvanphi19 <duongvanphi19@gmail.com>
2022-11-12 14:33:10 +0700

    add layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 
-export default function ({children}){
+export default function ({children}) {
    return (
-      <Head>
-         <title>Next Note</title>
-         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-      </Head>
-      {children}
-      <footer>Footer</footer>
+      <>
+         <Head>
+            <title>Next Note</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+         </Head>
+         {children}
+         <footer>Footer</footer>
+      </>
    )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import {AnimatePresence} from 'framer-motion'
 import {ChakraProvider} from '@chakra-ui/react'
-import Layout from "./components/layout'
+import Layout from '../components/layout'
 function MyApp({Component, pageProps}) {
    return (
       <AnimatePresence>
