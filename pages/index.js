07e5da46495a1b17fdded0ee330cc9b89904a892
duongvanphi19 <duongvanphi19@gmail.com>
2022-11-12 16:25:21 +0700

    add editor.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
-import dynamic from 'next/dynamic'
-import {convertFromRaw} from 'draft-js'
-const Editor = dynamic(() => import('react-draft-wysiwyg').then(mod => mod.Editor), {ssr: false})
+//import dynamic from 'next/dynamic'
+//import {convertFromRaw} from 'draft-js'
+//const Editor = dynamic(() => import('react-draft-wysiwyg').then(mod => mod.Editor), {ssr: false})
 
 import {
    Container,
@@ -12,14 +12,15 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
 
 export default function Home() {
-   const [contentState, setContentState] = useState(convertFromRaw({}))
+   //const [contentState, setContentState] = useState(convertFromRaw({}))
 
    return (
       <Container>
          <Heading mb={6}>Editor</Heading>
-      <Editor 
+      {/*<Editor 
       onContentStateChange={setContentState}
       />
+      */}
       </Container>
    )
 }
