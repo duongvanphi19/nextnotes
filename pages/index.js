d4dd88e3baaa199c14010a03c1fce5e55acaa4ad
glasscss <duongvphi196@gmail.com>
2023-05-17 23:06:21 +0700

    a

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
 import {useState} from 'react'
-//import dynamic from 'next/dynamic'
-//import {convertFromRaw} from 'draft-js'
-//const Editor = dynamic(() => import('react-draft-wysiwyg').then(mod => mod.Editor), {ssr: false})
+import dynamic from 'next/dynamic'
+import {convertFromRaw} from 'draft-js'
+const Editor = dynamic(() => import('react-draft-wysiwyg').then(mod => mod.Editor), {ssr: false})
 
 import {
    Container,
-   Heading
+   Heading,
+   
 } from '@chakra-ui/react'
 
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
 
 export default function Home() {
-   //const [contentState, setContentState] = useState(convertFromRaw({}))
+   const [contentState, setContentState] = useState(null)
 
    return (
       <Container>
-         <Heading mb={6}>Editor</Heading>
-      {/*<Editor 
+         <Heading mb={6}>Editor 1</Heading>
+      <Editor 
       onContentStateChange={setContentState}
       />
-      */}
+
+      <textarea>{JSON.stringify(contentState)}</textarea>
+      
       </Container>
    )
 }
